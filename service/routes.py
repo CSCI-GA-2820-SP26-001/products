69f6626c539b063b4bd51569fc2de9e56c260eeb
allen-ajith <allen200118@gmail.com>
2026-05-04 16:13:41 -0400

    Fix stale docstring in routes.py

--- service/routes.py
+++ service/routes.py
@@ -15,10 +15,10 @@
 ######################################################################
 
 """
-YourResourceModel Service
+Product Catalog Service
 
 This service implements a REST API that allows you to Create, Read, Update
-and Delete YourResourceModel
+and Delete Products
 """
 from decimal import Decimal, InvalidOperation
 from flask import jsonify, request, url_for, abort, render_template
